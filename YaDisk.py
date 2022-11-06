e484d2c8b43b529b36655d83ed24121f96deeb56
Elek <Elek2@yandex.ru>
2022-11-06 11:44:41 +0300

    Второе изменение

--- YaDisk.py
+++ YaDisk.py
@@ -6,40 +6,18 @@ class YaUploader:
 	def __init__(self, token):
 		self.token = token
 
-	# file_path: str
-	def upload(self):
+	def upload(self, file_path: str):
 		upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 		headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
-		params = {"path": '', "overwrite": "true"}
+		file_name = file_path.split("\\")[-1]
+		params = {"path": file_name, "overwrite": True}
 		response = requests.get(upload_url, headers=headers, params=params)
-		print(response)
-
-	# def get_files_list(self):
-	# 	files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
-	# 	headers = self.get_headers()
-	# 	response = requests.get(files_url, headers=headers)
-	# 	return response.json()
-
-	# def _get_upload_link(self, disk_path):
-	# 	upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-	# 	headers = self.get_headers()
-	#
-	# 	response = requests.get(upload_url, headers=headers, params=params)
-	# 	pprint(response.json())
-	# 	return response.json()
-	#
-	# def upload_files(self, disk_path, file_name):
-	# 	result = self._get_upload_link(disk_path=disk_path)
-	# 	href = result.get('href', '')
-	# 	response = requests.put(href, data=open(file_name, 'rb'))
-	# 	response.raise_for_status()
-	# 	if response.status_code == 201:
-	# 		print("Success")
-
+		pprint(response.json())
+		disk_link = response.json()['href']
+		requests.put(disk_link, open(file_name, 'rb'))
 
 
 TOKEN = 'y0_AgAAAAAAlde3AADLWwAAAADTHHzbYD0Iis_mQIyLneHfVdTHZJwtbnw'
-ya = YaUploader(token=TOKEN)
-ya.upload()
-
-
+ya = YaUploader(TOKEN)
+path = r'D:\PyProject\Netology\Requests\test2.txt'
+ya.upload(path)
